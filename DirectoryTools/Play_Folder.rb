4e2155838903b918ebf50084f212b9795068e102
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-10-17 14:42:31 -0400

    Play directory plugin. Clears playlist, then plays directory recursively.

---
+++ DirectoryTools/Play_Folder.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+#Plays all songs in specified folder
+require '../config'
+
+ENV['REQUEST_URI'] =~ /\?(.*)$/
+dir = $1.url_decode
+
+$mpd.clear
+$mpd.add dir
+$mpd.play 0
+
+print "Location: ../browse.rb?#{dir.url_encode}\r\n"
+print "\r\n"
