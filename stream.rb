c42dc7d6fde2f13e92c77ca812535475be2c1ff2
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-10-19 17:11:07 -0400

    Seems to send the file correctly, but quiktime wont play stream. HTTP headers, maybe?

--- stream.rb
+++ stream.rb
@@ -1,3 +1,18 @@
 #!/usr/bin/ruby
 #Gives access to music library files over HTTP
 require 'config'
+
+ENV['REQUEST_URI'] =~ /\?(.*)$/
+f = $config[:mdir] + '/' + $1.url_decode
+f = File.open f, 'rb'
+
+print "Content-Type: audio/mpeg\r\n"#TODO: Make it guess mime type
+print "\r\n"
+
+#Outputs file
+while blk = f.read(4096)
+    $stdout.print blk
+    $stdout.flush
+end
+
+f.close
