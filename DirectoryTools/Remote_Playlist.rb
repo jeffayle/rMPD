8959aa1db0f3c38577257f9411ce44b750946c5f
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-10-19 20:10:14 -0400

    Creates a playlist for a directory that can be played back remotely

---
+++ DirectoryTools/Remote_Playlist.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+#Gives the user an M3U playlist with remote URLs for streaming
+require '../config'
+
+ENV['REQUEST_URI'] =~ /\?(.*)$/
+dir = $1.url_decode
+
+#Get files
+files = $mpd.files dir
+
+print "Content-Type: audio/x-mpegurl\r\n"
+print "\r\n"
+
+puts "# Directory: #{dir}"
+files.each do |s|
+     puts $config[:httpAddr] + '/stream.rb?' + s.url_encode
+end
+
+##TODO: Add extra metadata
