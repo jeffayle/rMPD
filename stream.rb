43160a71fc681933c23427260e58698d47c6b503
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-10-20 22:23:33 -0400

    Made stream exit if target file contains /../ to not expose whole filesystem

--- stream.rb
+++ stream.rb
@@ -4,6 +4,11 @@ require 'config'
 
 ENV['REQUEST_URI'] =~ /\?(.*)$/
 f = $config[:mdir] + '/' + $1.url_decode
+
+if f =~ /\/\.\.\//
+    exit #If url contains /../, exit to not expose filesystem
+end
+
 f = File.open f, 'rb'
 
 print "Content-Type: audio/mpeg\r\n"#TODO: Make it guess mime type
