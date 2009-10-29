938bd7ed33173523e2536df4359762ae445651e8
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-10-29 16:48:08 -0400

    Added configuration for playlist columns

--- config.rb
+++ config.rb
@@ -13,6 +13,26 @@ $config[:mdir] = '/exthd/jeff/Music'
 #Make sure that any computer that will be using it can access it on this url.
 $config[:httpAddr] = 'http://192.168.1.134/~mpdr'
 
+#Playlist view columns
+## Headers
+$config[:plHeaders] = [
+    '#', 'Title', 'Artist', 'Album', ''
+]
+## Contents
+#Valid options are: artist, composer, track, title, time, id, date, album,
+#   genre, file,  pos & tools
+#All are the metadata in the audio file except for:
+#id is the unique identification number given by MPD
+#file is the filename relative to the root of your music directory
+#pos is the position in the playlist, 0 is the first song
+#Tools is an extra column with stuff to edit the playlist
+#
+#Each will be replaced with just the corresponding text, except for title,
+#which will include a link to skip to that song
+$config[:plContent] = [
+    'track', 'title', 'artist', 'album', 'tools'
+]
+
 #Connection stuff
 begin
     require 'librmpd'
