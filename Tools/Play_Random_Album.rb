df186c35fb4cc45319d10926842ec70a6465ad4e
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-11-24 13:23:07 -0500

    generates a list of upcoming albums to play, instead of choosing randomly each run (**Untested)

--- Tools/Play_Random_Album.rb
+++ Tools/Play_Random_Album.rb
@@ -2,9 +2,29 @@
 #Clears the current playlist, then plays a random album
 require '../common'
 
-all = $mpd.albums
-album = all[(rand*all.length).floor]
+#Get list of upcoming albums
+nexts = nil
+if File.file? '../State/Play_Random_Album'
+    nexts = File.read('../State/Play_Random_Album').split("\n")
+    nexts = nil if nexts.length == 0
+end
+unless nexts
+    all = $mpd.albums
+    nexts = Array.new
+    25.times do
+       n = (rand * all.length).floor
+       nexts << all.delete(n)
+    end
+end
+
+album = all.delete(0)
 songs = $mpd.find 'album', album
+
+#Writes out new list of albums
+f = File.open '../State/Play_Random_Album', 'w'
+f.write(nexts.join("\n"))
+f.flush
+f.close
 
 #Clears the playlist, then queues those songs
 $mpd.clear
