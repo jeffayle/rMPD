d5fd980fb8dac06c651383494b8de9e39a5f8b83
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-12-26 15:53:11 -0500

    Display a progress bar of how far into the playlist has been played.

--- index.rb
+++ index.rb
@@ -16,6 +16,7 @@ $playlist = $mpd.playlist
 
 #Get length of playlist
 $total_length = $playlist.inject(0){|m,c| m+c['time'].to_i }
+
 #Figure out how much of that is left
 if $stopped
     $remaining_length = 0
@@ -23,6 +24,9 @@ else
     $remaining_length = $playlist[0...$current].inject(0){ |m, c|
         m + c['time'].to_i } + $mpd.time[0]
 end
+
+#Figure out how wide the playlist progress bar should be.
+$pbPlayingWidth = 100.0 * $remaining_length / $total_length
 
 #Get tools
 $tools =  Dir.entries('Tools/').select{|d| d[0,1] != '.'}.sort_nocase
