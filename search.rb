1719e67e2b43e411115e5bfd6b8b58504fed85c8
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-12-26 21:29:40 -0500

    Searching (no results)

---
+++ search.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+#Searches for files in the database
+require 'common'
+require 'erb'
+
+print "Content-Type: text/html\r\n"
+print "\r\n"
+
+params = url_params ENV['REQUEST_URI']
+$term = (params['search'] or '')
+$type = params['type']
+p $term
+
+$type = 'title ' unless ['title','artist','album','filename'].include? $type
+$searching = ($term != '')
+p $searching
+
+if $searching
+    $results = $mpd.search $type, $term
+end
+
+out = ERB.new(File.read('views/search.html'))
+print out.result
